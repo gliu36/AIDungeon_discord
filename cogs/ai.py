94b300801c9e2f172ded2ddb75388c98fd62deed
gliu36 <gerryliu36@gmail.com>
2020-05-25 08:55:00 -0400

    AIDungeon is imported correctly and opening is played

--- cogs/ai.py
+++ cogs/ai.py
@@ -18,14 +18,21 @@ class AI(commands.Cog):
 
     @commands.is_owner()
     async def play_dungeon(self, ctx, channel):
-        await channel.send("Initializing AI Dungeon! (This might take a few minutes)")
+        await channel.send("```Initializing AI Dungeon! (This might take a few minutes)```")
 
         generator = GPT2Generator()
         story_manager = UnconstrainedStoryManager(generator)
 
         with open(r"AIDungeon/opening.txt", "r", encoding="utf-8") as f:
             starter = f.read()
-        await channel.send(starter)
+        await channel.send(f"```{starter}```")
+
+        def check(m):
+            return m.channel == channel and (m.content == '0' or m.content == '1')
+
+        await channel.send("```0) New Game\n\n1) Load Game```")
+        msg = await ctx.bot.wait_for('message', check=check)
+        await channel.send(f'You said {msg.content}')
 
 
 def setup(bot):
